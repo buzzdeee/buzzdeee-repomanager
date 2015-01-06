f42340ec78f89e1192fc76682f29fae6527781cf
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2015-01-06 10:12:13 +0100

    fixup default installpath for OpenBSD

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class repomanager::params {
   $obsdpkgconfdefaults = {
     fullwidth   => 'yes',
     loglevel    => '1',
-    installpath => "http://ftp.openbsd.org/pub/OpenBSD/${kernelmajversion}/${hardwaremodel}/",
+    installpath => "http://ftp.openbsd.org/pub/OpenBSD/${kernelmajversion}/packages/${hardwaremodel}/",
     nochecksum  => 'no',
     ntogo       => 'yes',
   }
