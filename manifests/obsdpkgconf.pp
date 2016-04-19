16598ce23e8100e67dbd97feee0ba0eea00a1cff
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-04-19 11:03:03 +0200

    Add installation of mtier stable keys based on the kernelversion of the node

--- manifests/obsdpkgconf.pp
+++ manifests/obsdpkgconf.pp
@@ -8,6 +8,14 @@ class repomanager::obsdpkgconf (
     $pkgconfig = $configdefaults
   }
 
+  $majversion = $::os[release][major]
+  $minversion = $::os[release][minor]
+  $version = "${majversion}${minversion}"
+  exec { 'fetch_mtier_pubkey':
+    command => "/usr/bin/ftp -o /etc/signify/mtier-${version}-pkg.pub https://stable.mtier.org/mtier-${version}-pkg.pub",
+    creates => "/etc/signify/mtier-${version}-pkg.pub"
+  }
+
   file { '/etc/pkg.conf':
     owner   => 'root',
     group   => '0',
